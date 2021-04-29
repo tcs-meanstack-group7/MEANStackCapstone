b815b9dd14a81cd31fd56241f510d50430fcb683
nikcollina <nikolas.collina@gmail.com>
2021-04-28 20:41:03 -0400

    update order wirking

--- angular-frontend/src/app/emp-update-order/emp-update-order.component.ts
+++ angular-frontend/src/app/emp-update-order/emp-update-order.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../Order.service';
+import { Order } from '../Order.model';
 
 @Component({
   selector: 'app-emp-update-order',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./emp-update-order.component.css']
 })
 export class EmpUpdateOrderComponent implements OnInit {
-
-  constructor() { }
-
+  orders = new Array<Order>()
+  constructor(public orderService:OrderService) { }
+  msg=""
   ngOnInit(): void {
+    this.orderService.GetAllOrders().subscribe((result:any)=>{
+      this.orders = result;
+      console.log(result)
+    },
+      (error:any)=>{
+        console.log(error);
+        //this.msg=error;
+        
+      })
+  }
+  updateOrder(orderRef:any){
+    let body = {"id":orderRef.id,"status":orderRef.status}
+    this.orderService.UpdateOrder(body).subscribe((result:any)=>{
+      this.orders = result;
+      console.log(result)
+      this.msg=result.Response
+    },
+      (error:any)=>{
+        console.log(error);
+        this.msg=error;
+        
+      })
   }
 
 }
